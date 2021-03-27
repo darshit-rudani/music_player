cf7ac36a42f3f3ecb487699364d6ff79264572e5
Darshit Rudani <darshit112001@users.noreply.github.com>
2021-03-28 00:00:58 +0530

    change theme to light

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
+      theme: ThemeData.light(),
       home: Musicplayer(),
     );
   }
