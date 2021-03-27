9816e86de6400ef88a55cd6acf595fcf53153187
Darshit Rudani <darshit112001@users.noreply.github.com>
2021-03-27 23:33:58 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Musicplayer(),
+    );
+  }
+}
+
+
+class Musicplayer extends StatefulWidget {
+  @override
+  _MusicplayerState createState() => _MusicplayerState();
+}
+
+class _MusicplayerState extends State<Musicplayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
